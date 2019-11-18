0c977716c15a6e22c7104fb86de8512bf31951ea
hyh <fjnyhyh@163.com>
2019-11-18 22:02:32 +0800

    悬浮窗权限，录屏权限，录音权限管理

--- app/src/main/java/com/hyh/floatingviewapp/MainActivity.java
+++ app/src/main/java/com/hyh/floatingviewapp/MainActivity.java
@@ -1,58 +1,69 @@
 package com.hyh.floatingviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.M;
+
 public class MainActivity extends AppCompatActivity {
+    private PermissionManager mPermissionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPermissionManager = new PermissionManager(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(mPermissionManager!=null){
+            mPermissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0) {
-            if (FloatPermission.checkPermissionAbove6(this)) {
-                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
-                startService(new Intent(MainActivity.this, FloatingButtonService.class));
-            } else {
-                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
-            }
+        if(mPermissionManager!=null){
+            mPermissionManager.onActivityResult(requestCode,resultCode,data);
         }
         super.onActivityResult(requestCode,resultCode,data);
     }
 
-    public void startFloatingButtonService(View view) {
-        if (FloatingButtonService.isStarted) {
-            return;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //6以上机型需要动态授权悬浮窗权限
-            if(!Settings.canDrawOverlays(this)){
-                startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0);
-            }else{
-                startService(new Intent(MainActivity.this, FloatingButtonService.class));
-            }
-        }else {
-            //6以下机型默认会授权悬浮窗权限，但有些rom会有问题，所以这里通过反射去判断是否已经授权
-            if(FloatPermission.checkOp(this)){
-                startService(new Intent(MainActivity.this, FloatingButtonService.class));
-            }else{
-                //未授权跳转到对应机型权限设置页面，需要适配不同机型
-            }
+    public void start(View view){
+        if(mPermissionManager!=null){
+            mPermissionManager.start();
         }
     }
 
+//    public void startFloatingButtonService() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            //6以上机型需要动态授权悬浮窗权限
+//            if(!Settings.canDrawOverlays(this)){
+//                startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), REQUEST_FLOATING_PERMISSIONS);
+//            }else{
+//                startFloating();
+//            }
+//        }else {
+//            //6以下机型默认会授权悬浮窗权限，但有些rom会有问题，所以这里通过反射去判断是否已经授权
+//            if(FloatPermission.checkOp(this)){
+//                startFloating();
+//            }else{
+//                //未授权跳转到对应机型权限设置页面，需要适配不同机型
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/hyh/floatingviewapp/PermissionManager.java
@@ -0,0 +1,202 @@
+package com.hyh.floatingviewapp;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.content.Context.MEDIA_PROJECTION_SERVICE;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * created by curdyhuang on 2019-11-18
+ */
+public class PermissionManager {
+    private static final int REQUEST_AUDIO_PERMISSIONS = 0;//请求录音权限
+    private static final int REQUEST_FLOATING_PERMISSIONS = 1;//请求悬浮窗权限
+    private static final int REQUEST_MEDIA_PROJECTION = 2;//请求截屏权限
+
+    private MediaProjectionManager mMediaProjectionManager;
+    private MediaProjection mMediaProjection;
+
+    private Activity mHost;
+
+    @TargetApi(LOLLIPOP)
+    public PermissionManager(@NonNull Activity host) {
+        mHost = host;
+        //android 5.0以上才支持
+        mMediaProjectionManager = (MediaProjectionManager) host.getApplicationContext().getSystemService(MEDIA_PROJECTION_SERVICE);
+    }
+
+    public void start(){
+        //检查录音权限
+        if (checkAudioPermissions()) {
+            //检查悬浮窗权限
+            if(checkFloatingPermission()){
+                //检查录屏权限
+                if(checkRecordPermission()){
+                    startWork(mMediaProjection);
+                }else{
+                    //请求录屏权限
+                    requestMediaProjection();
+                }
+            }else{
+                //请求悬浮窗权限
+                requestFloatingPermissions();
+            }
+        } else if (Build.VERSION.SDK_INT >= M) {
+            //请求录音权限
+            requestAudioPermissions();
+        }
+    }
+
+    /**
+     * 检查录音权限
+     * @return
+     */
+    private boolean checkAudioPermissions() {
+        PackageManager pm = mHost.getPackageManager();
+        if(pm != null) {
+            String packageName = mHost.getPackageName();
+            int granted = pm.checkPermission(RECORD_AUDIO, packageName);
+            return granted == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    /**
+     * 检查悬浮窗权限
+     * @return
+     */
+    private boolean checkFloatingPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return Settings.canDrawOverlays(mHost);
+        }else {
+            //6以下机型默认会授予悬浮窗权限，但有些rom会有问题，所以这里通过反射去判断是否已经授权
+            return FloatPermission.checkOp(mHost);
+        }
+    }
+
+    /**
+     * 检查录屏权限
+     * @return
+     */
+    private boolean checkRecordPermission(){
+        return mMediaProjection != null;
+    }
+
+    /**
+     * 请求录音权限
+     */
+    @TargetApi(M)
+    private void requestAudioPermissions() {
+        mHost.requestPermissions(new String[]{RECORD_AUDIO}, REQUEST_AUDIO_PERMISSIONS);
+    }
+
+    /**
+     * 请求悬浮窗权限
+     */
+    private void requestFloatingPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //6以上机型需要动态申请悬浮窗权限
+            mHost.startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + mHost.getPackageName())), REQUEST_FLOATING_PERMISSIONS);
+        }else {
+            //6以下机型未授权跳转到对应机型权限设置页面，需要适配不同机型
+        }
+    }
+
+    /**
+     * 请求录屏权限
+     */
+    @TargetApi(LOLLIPOP)
+    private void requestMediaProjection() {
+        if(mMediaProjectionManager!=null) {
+            Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+            mHost.startActivityForResult(captureIntent, REQUEST_MEDIA_PROJECTION);
+        }
+    }
+
+    private void startWork(MediaProjection mediaProjection){
+        startCapturing(mediaProjection);
+        startFloating();
+    }
+
+    /**
+     * 开始录屏
+     * @param mediaProjection
+     */
+    private void startCapturing(MediaProjection mediaProjection) {
+        if (checkAudioPermissions()) {
+        } else {
+        }
+    }
+
+    /**
+     * 开始显示悬浮窗
+     */
+    private void startFloating(){
+        if (FloatingButtonService.isStarted) {
+            return;
+        }
+        mHost.startService(new Intent(mHost, FloatingButtonService.class));
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_AUDIO_PERMISSIONS) {
+            int granted = PackageManager.PERMISSION_GRANTED;
+            for (int r : grantResults) {
+                granted |= r;
+            }
+            if (granted == PackageManager.PERMISSION_GRANTED) {
+                if(checkFloatingPermission()){
+                    if (checkRecordPermission()) {
+                        startWork(mMediaProjection);
+                    } else {
+                        //请求录屏权限
+                        requestMediaProjection();
+                    }
+                }else{
+                    //请求悬浮窗权限
+                    requestFloatingPermissions();
+                }
+            }else{
+                Toast.makeText(mHost, "授权录音权限失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @TargetApi(LOLLIPOP)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_FLOATING_PERMISSIONS) {
+            if (FloatPermission.checkPermissionAbove6(mHost)) {
+                if(checkRecordPermission()){
+                    startWork(mMediaProjection);
+                }else{
+                    //请求录屏权限
+                    requestMediaProjection();
+                }
+            } else {
+                Toast.makeText(mHost, "授权悬浮窗权限失败", Toast.LENGTH_SHORT).show();
+            }
+        }else if (requestCode == REQUEST_MEDIA_PROJECTION&&mMediaProjectionManager!=null) {
+            MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+            if (mediaProjection == null) {
+                Toast.makeText(mHost, "授权录屏权限失败", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mMediaProjection = mediaProjection;
+            startWork(mMediaProjection);
+        }
+    }
+
+}
